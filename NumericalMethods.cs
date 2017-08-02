2b996564f9de8566a0b777dc75e5c29db5ac8f27
Paul Perrone Jr <30580864+pperronejr@users.noreply.github.com>
2017-08-01 21:19:15 -0400

    Initial Upload

---
+++ NumericalMethods.cs
@@ -0,0 +1,36 @@
+﻿
+//
+// Copyright 2017 Paul Perrone.  All rights reserved.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IDA.Numeric
+{
+    public static class NumericalMethods
+    {
+        static public List<double> GetQuadraticRoots(double a, double b, double c)
+        {
+            if (a != 0.0)
+            {
+                List<double> roots = new List<double>();
+                double discriminant = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+                if (discriminant == 0.0)
+                    roots.Add(-b / (2.0 * a));
+                else if (discriminant > 0.0)
+                {
+                    roots.Add((-b + Math.Sqrt(discriminant)) / (2.0 * a));
+                    roots.Add((-b - Math.Sqrt(discriminant)) / (2.0 * a));
+                }
+
+                return roots;
+            }
+            else
+                throw new System.ApplicationException("Quadratic coefficient a is equal to 0.0");
+        }
+    }
+}
---
+++ TestNumeric.cs
@@ -0,0 +1,38 @@
+﻿
+//
+// Copyright 2017 Paul Perrone.  All rights reserved.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IDA.Numeric
+{
+    public class TestNumeric
+    {
+        public static void Main(string[] args)
+        {
+            // Answers should be 1.0 and -3.0
+            List<double> doubleRoots = NumericalMethods.GetQuadraticRoots(1.0, 2.0, -3.0);
+
+            // Answers should be -1.0
+            List<double> singleRoots = NumericalMethods.GetQuadraticRoots(1.0, 2.0, 1.0);
+
+            // No roots since they are imaginary
+            List<double> noRoots = NumericalMethods.GetQuadraticRoots(1.0, 1.0, 1.0);
+
+            // Should throw an error since a=0.0
+            string errorMsg = "";
+            try
+            {
+                List<double> errorRoots = NumericalMethods.GetQuadraticRoots(0.0, 2.0, 1.0);
+            }
+            catch (System.Exception e)
+            {
+                errorMsg = e.Message;
+            }   
+        }
+    }
+}
